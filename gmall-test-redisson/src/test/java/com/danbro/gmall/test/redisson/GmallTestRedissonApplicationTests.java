596c40d4b7a2379cefb811c55933ab3be6583ac6
danbro <710170342@qq.com>
2019-11-19 11:15:54 +0800

    调整maven里test模块冲突

--- gmall-test-redisson/src/test/java/com/danbro/gmall/test/redisson/GmallTestRedissonApplicationTests.java
+++ gmall-test-redisson/src/test/java/com/danbro/gmall/test/redisson/GmallTestRedissonApplicationTests.java
@@ -1,6 +1,7 @@
 package com.danbro.gmall.test.redisson;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
